25eca9ae48dd99414825fe1c550a45de26aeb518
Hong Tan <h_tan@ucsb.edu>
2022-11-08 20:07:52 -0800

    mp4 -> mp3, more comments for ease of understanding

--- youtube_to_mp3.py
+++ youtube_to_mp3.py
@@ -13,19 +13,29 @@ browser.get('https://en.y2mate.is/95/')
 
 time.sleep(2)
 
-link = browser.find_element_by_id('txtUrl')
-link.send_keys('https://www.youtube.com/watch?v=qZ90POH8JlA')
+youtube_link = 'https://www.youtube.com/watch?v=qZ90POH8JlA'
 
+# Type link into input field
+link = browser.find_element_by_id('txtUrl')
+link.send_keys(youtube_link)
+
+# Go into audio (mp3) section
+audio = browser.find_element_by_xpath('/html/body/section[1]/div/div[2]/div[2]/div/div[2]/div/div[1]/button[2]')
+audio.click()
+
+# Start the conversion to mp3
 start = browser.find_element_by_xpath('/html/body/section[1]/div/div[2]/div[1]/div/form/button/span[1]')
 start.click()
 
 time.sleep(2)
 
+# Click the convert button for 160k quality
 btn = browser.find_element_by_xpath('/html/body/section[1]/div/div[2]/div[2]/div/div[2]/div/div[2]/table/tbody/tr[2]/td[3]/button')
 btn.click()
 
 time.sleep(1)
 
+# Click the download button
 downloadbtn = browser.find_element_by_xpath('/html/body/section[1]/div/div[2]/div[2]/div/div[2]/div/div[2]/table/tbody/tr[2]/td[3]/button/a')
 downloadbtn.click()
 
