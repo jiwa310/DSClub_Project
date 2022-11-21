071985663c5dc057adc70a6cc481e3afdfb724fe
RealWeston34 <73916480+RealWeston34@users.noreply.github.com>
2022-11-21 14:57:27 -0800

    Add files via upload

--- get_links.py
+++ get_links.py
@@ -2,22 +2,40 @@ from bs4 import BeautifulSoup as bs
 import requests
 from selenium import webdriver
 import time
+import csv
+import numpy as np
+import os
+
+#Change the working directory 
+path = input("Please enter the path: ")
+os.chdir(path)
+#Take user input to search for audio file
+search = input("Search for audio files related to: ").replace(" ", "+")
 
 driver =webdriver.Chrome()
-url = "https://www.youtube.com/results?search_query=angry+person"
+
+try:
+    url = "https://www.youtube.com/results?search_query=" + search
+except:
+    print("Invalid Search")
+
 driver.get(url)
 for i in range(10):
     time.sleep(1)
     driver.execute_script(f"window.scrollTo(0,10000*{i})")
+
 soup = bs(driver.page_source, 'html.parser')
 driver.quit()
 res = soup.find_all('a', id = 'thumbnail')
-print(res)
-count = 0
+
 for thumbnail in res:
     if(thumbnail.get("href")):
-       count += 1
-print(count)
+        os.system("yt-dlp -x --audio-format mp3 https://www.youtube.com" + thumbnail.get("href"))
+
+        
+
+
+
 
 
 
