9867be968a14eb73117e30d43772af1eb7bda815
Hong Tan <h_tan@ucsb.edu>
2023-02-14 20:35:26 -0800

    website with working mic and download

--- text-analysis/pipeline.py
+++ text-analysis/pipeline.py
@@ -2,7 +2,7 @@ import whisper
 from transformers import pipeline
 
 w_model = whisper.load_model("base")
-result = w_model.transcribe("speech-to-text/audio-test-files/insane.wav")
+result = w_model.transcribe("/Users/hongtan/Desktop/DSClub_Project/speech-to-text/audio-test-files/my_recording.wav")
 text = result["text"]
 
 print(text)
--- website/recordAudio.js
+++ website/recordAudio.js
@@ -1,67 +1,17 @@
-import './App.css';
-import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AudioRecorder } from 'react-audio-voice-recorder';
 
-let gumStream = null;
-let recorder = null;
-let audioContext = null;
+const addAudioElement = (blob) => {
+  const url = URL.createObjectURL(blob);
+  const audio = document.createElement("audio");
+  audio.src = url;
+  audio.controls = true;
+  document.body.appendChild(audio);
+};
 
-function RecorderJSDemo() {
-    const startRecording = () => {
-        let constraints = {
-            audio: true,
-            video: false
-        }
-
-        audioContext = new window.AudioContext();
-        console.log("sample rate: " + audioContext.sampleRate);
-
-        navigator.mediaDevices
-            .getUserMedia(constraints)
-            .then(function (stream) {
-                console.log("initializing Recorder.js ...");
-
-                gumStream = stream;
-
-                let input = audioContext.createMediaStreamSource(stream);
-
-                recorder = new window.Recorder(input, {
-                    numChannels: 1
-                })
-
-                recorder.record();
-                console.log("Recording started");
-            }).catch(function (err) {
-        });
-    }
-
-    const stopRecording = () => {
-        console.log("stopButton clicked");
-
-        recorder.stop();
-        gumStream.getAudioTracks()[0].stop();
-
-        recorder.exportWAV(onStop);
-    }
-
-    const onStop = (blob) => {
-        console.log("uploading...");
-
-        let data = new FormData();
-
-        data.append('text', "this is the transcription of the audio file");
-        data.append('wavfile', blob, "recording.wav");
-
-        const config = {
-            headers: {'content-type': 'multipart/form-data'}
-        }
-    }
-
-    return (
-        <div>
-            <button onClick={startRecording} type="button">Start</button>
-            <button onClick={stopRecording} type="button">Stop</button>
-        </div>
-    );
+export default function App() {
+  return <React.StrictMode>
+    <AudioRecorder onRecordingComplete={addAudioElement} />
+  </React.StrictMode>
 }
-
-export default RecorderJSDemo;
